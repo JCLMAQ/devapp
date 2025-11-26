73f5c7f44d5d6836efd3d15e030c7db7b29136a2
JCM <jcl.maquinay@gmail.com>
2025-11-26 13:39:25 +0100

    feat: add pnpm workspace configuration and prisma setup
    - Created pnpm-workspace.yaml to specify only built dependencies.
    - Added prisma.config.ts for Prisma configuration with dotenvx support.
    - Updated tsconfig.base.json to set baseUrl and define path for @devapp/prisma.

---
+++ prisma.config.ts
@@ -0,0 +1,22 @@
+import "@dotenvx/dotenvx/config";
+import path from "node:path";
+import { defineConfig, env } from "prisma/config";
+
+// dotenvx gère automatiquement l'expansion des variables
+
+export default defineConfig({
+  schema: path.join("libs/prisma/src/lib/prisma", "schema.prisma"),
+  migrations: {
+    path: path.join("libs/prisma/src/lib", "migrations"),
+    // seed: "tsx prisma/seed.ts",
+  },
+  views: {
+    path: path.join("libs/prisma/src/lib/views", "views"),
+  },
+  typedSql: {
+    path: path.join("libs/prisma/src/lib/queries", "queries"),
+  },
+  datasource: {
+    url: env("DATABASE_URL"),
+  }
+});
